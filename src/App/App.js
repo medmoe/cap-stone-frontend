0a68cbbc02b78da7ea0f98c8171e19a3c4bae1f3
ruimin <ruiminxu9478@gmail.com>
2021-01-26 11:43:22 -0500

    added a css styling to login form

--- src/App/App.js
+++ src/App/App.js
@@ -5,17 +5,18 @@ import Test from "./Test";
 import SideNav from "../components/SideNav/SideNavBar.jsx";
 import RecipeSearchBar from "../components/RecipeSearchBar/reciepeSearchBar";
 import Routes from "../components/Routes/routes";
+import FrontPageRoutes from "../components/Routes/FrontPageRoutes.jsx";
+import UserLoginView from "../components/UserForms/UserLoginView";
 
 const App = () => {
 	return (
 		<div className="Main-Container">
-			{/* <LoginPage/> */}
-			<div className="Main-ContainerWithSideNav">
+			{/* <FrontPageRoutes /> */}
+			<UserLoginView />
+			{/* <div className="Main-ContainerWithSideNav">
 				<SideNav />
 				<Routes />
-				{/* <UserProfile /> */}
-				{/* <RecipeSearchBar /> */}
-			</div>
+			</div> */}
 		</div>
 	);
 };
--- src/components/UserForms/UserLoginView.jsx
+++ src/components/UserForms/UserLoginView.jsx
@@ -1,120 +1,103 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import "./UserLoginView.css";
 
 const UserLoginView = () => {
-    
+	const [email, setemail] = useState("");
+	const [password, setpassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState({});
 
-    const UseFormLoginValidation = (formObject) => {
-        let errors = {};
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	let userLogin = {
+	// 		email,
+	// 		password,
+	// 	};
 
-        // const dispatch = useDispatch();
-        //Check if the email is empty. If it is empty, give the errors object the error String
-        if (!formObject.email) {
-            errors.email = "Email required!";
-        } else if (!/\S+@\S+\.\S+/.test(formObject.email)) {
-            //Check if the email is valid WITH THE "@" If it is not valid, give the errors object the error String
-            errors.email = "Please provide a valid email";
-        }
-    
-        //Check if the password is empty. If it is empty, give the errors object the error String
-        if (!formObject.password) {
-            errors.password = "Please provide a pasword";
-        } else if (formObject.password.length < 6) {
-            //Check if the password is 6 characters or longer. If it is short, give the errors object the error String
-            errors.password = "Password need to be longer than 6 characters";
-        }
-        
-        return errors;
-    };
+	// 	const isValid = UseFormLoginValidation(userLogin);
 
-    
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState({});
+	// 	let isThereAnyError = false; //This variable help us keep track whether or not the errors object is empty
 
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        let userLogin = {
-            email,
-            password,
-        }
+	// 	for (let element in isValid) {
+	// 		if (isValid.hasOwnProperty(element)) {
+	// 			//This will loop through the error object to find whether or not it is empty
+	// 			isThereAnyError = true;
+	// 		}
+	// 	}
 
-        
-        const isValid = UseFormLoginValidation(userLogin);
+	// 	if (isThereAnyError) {
+	// 		setErrorMessage(isValid);
+	// 	} else {
+	// 		setemail("");
+	// 		setpassword("");
+	// 		setErrorMessage({});
+	// 		console.log("it works");
+	// 		//do an axios post call to checkever or not the user is in database
+	// 		//get a response back from the database
+	// 		/*
+	//             if(the reponse saying the user does not exist)
+	//                 errors.usernotfound = "The user does not exist in the database"
+	//             else
+	//                 get the reponse of the user data from database and pass it to
+	//                 the currentUser REDUX
+	//                 dispatch(getTheUserFromDateBase(reponse));
+	//                 send  use to user profile
+	//         */
+	// 	}
+	// };
 
+	const handleChange = (e) => {
+		let { name } = e.target;
+		let { value } = e.target;
 
-        let isThereAnyError = false; //This variable help us keep track whether or not the errors object is empty
-    
-        for (let element in isValid) {
-            if (isValid.hasOwnProperty(element)) {
-                //This will loop through the error object to find whether or not it is empty
-                isThereAnyError = true;
-            }
-        }
-    
-        if (isThereAnyError) {
-            setErrorMessage(isValid);
-        } else {
-            setemail("");
-            setpassword("");
-            setErrorMessage({});
-            console.log("it works");
-            //do an axios post call to checkever or not the user is in database
-            //get a response back from the database
-            /*
-                if(the reponse saying the user does not exist)
-                    errors.usernotfound = "The user does not exist in the database"
-                else
-                    get the reponse of the user data from database and pass it to 
-                    the currentUser REDUX
-                    dispatch(getTheUserFromDateBase(reponse));
-                    send  use to user profile
-            */
-        }
-    
+		switch (name) {
+			case "email":
+				setemail(value);
+				break;
+			case "password":
+				setpassword(value);
+				break;
+			default:
+				break;
+		}
+	};
 
-    }
+	return (
+		<div className="LoginPageMainContainer">
+			<div className="LoginPageSubContainer">
+				<div className="LoginInFormImage"></div>
+				<form className="LoginForm">
+					<div className="loginFormItems">
+						<h1>Login In</h1>
+						<div className="emailContainer">
+							<label for="email"> Email:</label>
+							<input
+								type="text"
+								key="email"
+								name="email"
+								onChange={handleChange}
+								value={email}
+							></input>
+							<div>{errorMessage.email}</div>
+						</div>
 
+						<div className="passwordContainer">
+							<label for="password"> Password:</label>
+							<input
+								type="password"
+								id="password"
+								name="password"
+								onChange={handleChange}
+								value={password}
+							></input>
+							<div>{errorMessage.password}</div>
+						</div>
 
-
-    const handleChange = (e) => {
-        let {name} = e.target;
-        let {value} = e.target;
-
-        switch (name) {
-            case "email":
-                setemail(value);
-                break;
-            case "password":
-                setpassword(value);
-                break;
-            default:
-                break;
-        }
-    }
-    
-    
-    
-    
-    return(
-    <div>
-    <form onSubmit = {handleSubmit}>
-        <label for = "email" > Email:</label>
-        <input type ="text" key = "email" name = "email" onChange = {handleChange} value = {email}></input>
-        <div>{errorMessage.email}</div>
-        <br></br>
-
-        <label for = "password" > Password:</label>
-        <input type ="password" id = "password" name = "password" onChange = {handleChange} value = {password}></input>
-        <div>{errorMessage.password}</div>
-        <br></br>
-
-        <button>Submit</button>
-    </form>
-</div>
-)
-    
-}
+						<button>Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
 export default UserLoginView;
 //this gets sent to the index.js file in the views folder
