eacbe0416729afd1990725c7fdb0992178a7d302
ErickLojano <erickjlojano@gmail.com>
2021-01-26 10:41:10 -0500

    cleaning workspace

--- src/components/AllPlayers/containers/index.js
+++
@@ -1,4 +0,0 @@
-// Barrel file for container components, which will be used to compose our App component, which will be the root component we will pass to the ReactDOM render call;
-export {default as UserLoginContainer} from './UserLoginContainer';
-export {default as UserRegistrationContainer} from './UserRegistrationContainer';
-
--- src/components/AllPlayers/views/index.js
+++
@@ -1,3 +0,0 @@
-// Barrel file for presentational components, which will be used to compose our smart containers;
-export { default as UserLoginView } from './UserLoginView';
-export { default as UserRegistrationView} from './UserRegistrationView';
