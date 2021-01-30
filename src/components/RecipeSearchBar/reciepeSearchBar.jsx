d28a96a024609bcd57e15ebb255f14f5cecad831
ruimin <ruiminxu9478@gmail.com>
2021-01-30 12:57:19 -0500

    added new feature, user can now select in searching

--- src/components/RecipeSearchBar/reciepeSearchBar.jsx
+++ src/components/RecipeSearchBar/reciepeSearchBar.jsx
@@ -1,14 +1,47 @@
 import React, { useState } from "react";
 import "./reciepeSearchBar.css";
 import { useHistory } from "react-router-dom";
+//Component
+import SearchByAreaForm from "./searchByArea/searchByArea.jsx";
+import SearchByCategoryForm from "./searchByCategory/searchByCategoryForm";
+import SearchByNameForm from "./searchByName/searchByName";
 
 const RecipeSearchBar = () => {
 	const [search, setSearch] = useState("");
+	const [searchByCategory, setSearchByCategory] = useState(false);
+	const [searchByName, setSearchByName] = useState(true);
+	const [searchByArea, setSearchByArea] = useState(false);
 	const history = useHistory();
 
 	const HandleSubmit = (e) => {
 		e.preventDefault();
 		history.push(`/recipes/recipeid/${search}`);
+	};
+
+	const handleChangeForm = (e) => {
+		const { name } = e.target;
+		switch (name) {
+			case "searchName":
+				setSearchByName(true);
+				setSearchByCategory(false);
+				setSearchByArea(false);
+				break;
+			case "searchCategory":
+				setSearchByName(false);
+				setSearchByCategory(true);
+				setSearchByArea(false);
+				break;
+			case "searchArea":
+				setSearchByName(false);
+				setSearchByCategory(false);
+				setSearchByArea(true);
+				break;
+			default:
+				setSearchByName(true);
+				setSearchByCategory(false);
+				setSearchByArea(false);
+				break;
+		}
 	};
 
 	const handleQuery = (e) => {
@@ -18,23 +51,25 @@ const RecipeSearchBar = () => {
 	return (
 		<div className="RecipeSearchBarMainContainer">
 			<div className="RecipeSearchBarMiddleContainer">
-				<form className="barContainer" onSubmit={HandleSubmit}>
-					<div className="theBar">
-						<div className="barHeader">
-							<h1>Let's find your desired recipe</h1>
-						</div>
+				{searchByName ? (
+					<SearchByNameForm handleQuery={handleQuery} />
+				) : searchByCategory ? (
+					<SearchByCategoryForm handleQuery={handleQuery} />
+				) : (
+					<SearchByAreaForm handleQuery={handleQuery} />
+				)}
 
-						<input
-							className="searchBar"
-							placeholder="Enter a recipe name"
-							type="text"
-							onChange={handleQuery}
-							value={search}
-						></input>
-
-						<button>Search</button>
-					</div>
-				</form>
+				<div className="buttonContainer">
+					<button name="searchName" onClick={handleChangeForm}>
+						Search By Name
+					</button>
+					<button name="searchCategory" onClick={handleChangeForm}>
+						Search By Category
+					</button>
+					<button name="searchArea" onClick={handleChangeForm}>
+						Search By Area
+					</button>
+				</div>
 			</div>
 		</div>
 	);
---
+++ src/components/RecipeSearchBar/searchByName/searchByName.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SearchByNameForm = (props) => {
+	return (
+		<div>
+			<form>
+				<div className="theBar">
+					<div className="barHeader">
+						<h1>Let's find your desired recipe by name</h1>
+					</div>
+
+					<input
+						className="searchBar"
+						placeholder="Enter a recipe name"
+						type="text"
+						onChange={props.handleQuery}
+					></input>
+
+					<button>Search</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default SearchByNameForm;
