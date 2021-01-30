2f3ea7cd7d4d2e7577e85c0c14dc08745fbd3809
ruimin <ruiminxu9478@gmail.com>
2021-01-29 23:03:08 -0500

    first the login in session

--- src/components/UserForms/FormValidation/userFormSignUpValidation.js
+++ src/components/UserForms/FormValidation/userFormSignUpValidation.js
@@ -3,20 +3,19 @@ import axios from "axios";
 export const UseFormSignUpValidation = async (formObject) => {
 	let error = {};
 	//Check if the first name is empty. If it is empty, give the errors object the error String
-	if (!formObject.firstName) {
+	if (!formObject.first_name) {
 		error.firstname = "Firstname required!";
-	} else if (formObject.firstName.length < 2) {
+	} else if (formObject.first_name.length < 2) {
 		//Check if the first name is short. If it is short, give the errors object the error String
 		error.firstname = "Firstname must be longer than one character";
 	}
 
 	//Check if the first name is empty. If it is empty, give the errors object the error String
-	if (!formObject.lastName) {
+	if (!formObject.last_name) {
 		error.lastname = "Lastname required!";
-	} else if (formObject.lastName.length < 2) {
+	} else if (formObject.last_name.length < 2) {
 		//Check if the last name is short. If it is short, give the errors object the error String
 		error.lastname = "Lastname must be longer than one character";
-
 	}
 
 	//Check if the email is empty. If it is empty, give the errors object the error String
@@ -36,6 +35,7 @@ export const UseFormSignUpValidation = async (formObject) => {
 	}
 
 	if (Object.keys(error).length === 0 && error.constructor === Object) {
+		console.log(formObject);
 		const x = await axios.post(
 			"http://localhost:8080/api/users/register",
 			formObject
@@ -46,4 +46,3 @@ export const UseFormSignUpValidation = async (formObject) => {
 		return error;
 	}
 };
-
