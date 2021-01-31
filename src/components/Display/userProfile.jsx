09b402bda177fc9a6d3829704f47a24e4b5d968a
ruimin <ruiminxu9478@gmail.com>
2021-01-30 23:07:26 -0500

    made it cool

--- src/components/Display/userProfile.jsx
+++ src/components/Display/userProfile.jsx
@@ -5,51 +5,54 @@ import "./userProfile.css";
 import FavoriteRecipe from "./favoriteRecipe";
 
 const UserProfile = () => {
-	const currentUserInfo = useSelector(
-		(state) => state.currentUserReducer.currentUser
-	);
-	console.log("sadsadsadsadsa", currentUserInfo.recipes);
 	var d = new Date();
 	var date = d.getDate();
 	var month = d.getMonth() + 1;
 	var year = d.getFullYear();
 	var dataStr = month + "/" + date + "/" + year;
+	const token = localStorage.getItem("token");
+	const [user, setUser] = useState([]);
+	const [recipe, setRecipe] = useState([]);
+	useEffect(async () => {
+		const response = await axios.get(
+			`http://localhost:8080/api/users/session-id/${token}`
+		);
+		console.log(response);
+		setUser(response.data[0]);
+		setRecipe(response.data[0].recipes);
+	}, []);
 
 	return (
 		<div className="userProfileMainContainer">
-			{currentUserInfo.user ? (
-				<div className="userProfileInformationContainer">
-					<div className="userProfileUser">
-						<div className="userProfileImage"></div>
-						<div className="userProfileUserNameContainer">
-							<h2>{currentUserInfo.user.first_name}</h2>
-							<h2>{currentUserInfo.user.last_name}</h2>
-							<h2>Today's date </h2>
-							<h3>{dataStr}</h3>
-						</div>
-					</div>
-					<div className="userProfileData">
-						{currentUserInfo.recipes[2] ? (
-							<div>
-								<FavoriteRecipe recipe={currentUserInfo.recipes[0]} />
-								<FavoriteRecipe recipe={currentUserInfo.recipes[1]} />
-								<FavoriteRecipe recipe={currentUserInfo.recipes[2]} />
-							</div>
-						) : currentUserInfo.recipes[1] ? (
-							<div>
-								<FavoriteRecipe recipe={currentUserInfo.recipes[0]} />
-								<FavoriteRecipe recipe={currentUserInfo.recipes[1]} />
-							</div>
-						) : currentUserInfo.recipes[0] ? (
-							<div>
-								<FavoriteRecipe recipe={currentUserInfo.recipes[0]} />{" "}
-							</div>
-						) : null}
+			<div className="userProfileInformationContainer">
+				<div className="userProfileUser">
+					<div className="userProfileImage"></div>
+					<div className="userProfileUserNameContainer">
+						<h2>{user.first_name}</h2>
+						<h2>{user.last_name}</h2>
+						<h2>Today's date </h2>
+						<h3>{dataStr}</h3>
 					</div>
 				</div>
-			) : (
-				<div> loading </div>
-			)}
+				<div className="userProfileData">
+					{recipe[2] ? (
+						<div>
+							<FavoriteRecipe recipe={recipe[0]} />
+							<FavoriteRecipe recipe={recipe[1]} />
+							<FavoriteRecipe recipe={recipe[2]} />
+						</div>
+					) : recipe[1] ? (
+						<div>
+							<FavoriteRecipe recipe={recipe[0]} />
+							<FavoriteRecipe recipe={recipe[1]} />
+						</div>
+					) : recipe[0] ? (
+						<div>
+							<FavoriteRecipe recipe={recipe[0]} />{" "}
+						</div>
+					) : null}
+				</div>
+			</div>
 		</div>
 	);
 };
